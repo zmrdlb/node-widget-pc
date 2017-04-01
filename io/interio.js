8239681ec2153c597c792a8e7753f88f6f167168
zhangmingrui <592044573@qq.com>
2017-04-01 14:14:11 +0800

    新增io

---
+++ io/interio.js
@@ -0,0 +1,244 @@
+/**
+ * @fileoverview io接口请求控制器
+ * @version 1.0 | 2015-06-28 版本信息
+ * @author Zhang Mingrui | 592044573@qq.com
+ * @return 更多详细信息参考代码里对应定义方法或属性位置的注释说明
+ * 	transRequest {Function} 执行接口接口请求
+ *  transQueueRequest {Function} 对一组请求进行单独的队列控制依次请求。全部请求完毕后进行通知。
+ * @example
+ *
+ * 	 const Interio = require('libio-interio');
+ *
+ *   var basehost = 'http://127.0.0.1:8000';
+ *
+ * 	 Interio.transRequest({
+		 url: basehost+'/listdata',
+		 method:'POST',
+		 data: {'name': 'zmrdlb'}
+	 },{
+		 success: function(data){
+			 console.log(data);
+		 }
+		 // fail: function(){
+		 //     console.log('覆盖统一fail处理');
+		 // },
+		 // error: function(){
+		 //     console.log('覆盖统一error处理');
+		 // },
+		 // complete: function(){
+		 //     console.log('完成');
+		 // }
+	 });
+ * */
+const IoConfig = require('./ioconfig.js');
+
+//请求队列控制类
+function queueHandle(){
+	this.queue = []; //当前队列数据
+	this.inprocess = false; //当前队列接口是否正在处理
+	this.complete = function(){}; //队列请求完成后的回调
+};
+//执行队列数据请求
+queueHandle.prototype.advance = function(){
+	if(this.queue.length == 0){
+		this.inprocess = false;
+		if(typeof this.complete == 'function'){
+			this.complete();
+		}
+		return;
+	}
+	var req = this.queue.shift();
+	this.request(req,true);
+};
+/**
+* 添加接口请求处理
+* @param {JSON} *opt format后的接口参数
+* @param {Boolean} advance 是否是queueHander.advance调用
+*/
+queueHandle.prototype.request = function(opt,advance){
+	if(this.inprocess && !advance){
+		this.queue.push(opt);
+		return;
+	}
+	this.inprocess = true;
+	request(opt);
+};
+/**
+ * queueHandle对象控制器
+ */
+var queueControl = {
+	_queueobjs: [], //queueHandle对象列表
+	get: function(){ //返回当前空闲的queueHandle对象
+		var curqueue = null;
+		var queueobjs = this._queueobjs;
+		for(var i = 0, len = queueobjs.length; i < len; i++){
+			if(queueobjs[i].inprocess == false && queueobjs[i].lock == false){ //既无请求又没有被锁定
+				queueobjs[i].lock = true;
+				curqueue = queueobjs[i];
+				break;
+			}
+		}
+		if(curqueue == null){
+			curqueue = new queueHandle();
+			curqueue.lock = true;
+			this._queueobjs.push(curqueue);
+		}
+		return curqueue;
+	},
+	end: function(queue){ //通知当前queueHandle对象已经使用完毕
+		var queueobjs = this._queueobjs;
+		for(var i = 0, len = queueobjs.length; i < len; i++){
+			if(queueobjs[i] == queue){ //既无请求又没有被锁定
+				queueobjs[i].lock = false;
+				break;
+			}
+		}
+	}
+};
+/**
+ * 格式化io接口请求参数
+ * @param {JSON} *ioopt 数据接口参数
+ * param {JSON} *iocall io请求回调
+ * @param {queueHandle} *queueobj 队列控制器对象
+ */
+function format(ioargs,iocall,queueobj){
+	var _ioargs = {}, _iocall = {};
+	$.extend(true,_ioargs,IoConfig.ioargs,ioargs);
+	$.extend(true,_iocall,IoConfig.iocall,iocall);
+	IoConfig.format(_ioargs);
+	var oldsuccess = _iocall.success;
+	var oldcomplete = _iocall.complete;
+	var deallogin = _ioargs.customconfig.deallogin;
+	var dealfail = _ioargs.customconfig.dealfail;
+	var dealdata = _ioargs.customconfig.dealdata;
+	_iocall.success = function(data, textStatus, jqXHR){ //重写success方法，用来处理未登陆问题
+		if(deallogin && typeof IoConfig.login.filter == 'function'){ //监测是否有未登陆错误处理
+			if(IoConfig.login.filter(data)){ //未登录
+			    if(IoConfig.login.url != ''){ //跳转url
+			        var loginurl = IoConfig.login.url;
+                    var search = IoConfig.login.key+'='+encodeURIComponent(location.href);
+                    if(loginurl.lastIndexOf('?') != -1){
+                        loginurl = loginurl.replace(/\?/,'?'+search);
+                    }
+                    else{
+                        loginurl = loginurl+'?'+search;
+                    }
+                    location.href = loginurl;
+                    return;
+			    }else if(typeof IoConfig.login.deal == 'function'){
+			        IoConfig.login.deal(data);
+			        return;
+			    }
+			}
+		}
+		if(dealfail && typeof IoConfig.fail.filter == 'function'){ //检测是否有业务错误处理
+		    if(IoConfig.fail.filter(data)){ //业务错误
+		        if(typeof _iocall[IoConfig.fail.funname] == 'function'){
+		            _iocall[IoConfig.fail.funname](data, textStatus, jqXHR);
+		        }
+		    }else{ //业务成功
+		        if(dealdata){ //统一处理业务成功数据
+		            typeof oldsuccess == 'function' && oldsuccess(_ioargs.dealdata(data, textStatus, jqXHR), textStatus, jqXHR);
+		        }else{
+		            typeof oldsuccess == 'function' && oldsuccess(data, textStatus, jqXHR);
+		        }
+		    }
+		}else{
+		    typeof oldsuccess == 'function' && oldsuccess(data, textStatus, jqXHR);
+		}
+	};
+	if(_ioargs.customconfig.queue){ //说明接口同意进行队列控制
+		_iocall.complete = function(){ //重写接口请求完成事件
+			queueobj.advance();
+			typeof oldcomplete == 'function' && oldcomplete.apply(this,Array.prototype.slice.call(arguments));
+		};
+	}
+	return {
+		ioargs: _ioargs,
+		iocall: _iocall
+	};
+}
+function doajax(ioargs,iocall){
+	var interobj = null, getInter = ioargs.customconfig.getInter;
+	delete ioargs.customconfig;
+
+	interobj = $.ajax(ioargs).done(iocall.success).fail(iocall.error).always(iocall.complete)
+	if(interobj && typeof getInter == 'function'){
+		getInter(interobj);
+	}
+}
+/**
+ * 处理接口请求
+ * @param {JSON} ioopt format后的接口参数
+ */
+function request(ioopt){
+	var _ioargs = ioopt.ioargs;
+	var _iocall = ioopt.iocall;
+	var mode = _ioargs.customconfig.mode;
+	if(mode == 'ajax'){
+		if(_ioargs.dataType == undefined || _ioargs.dataType == ''){
+			_ioargs.dataType = 'json';
+		}
+		doajax(_ioargs,_iocall);
+	}else if(mode == 'jsonp'){
+		_ioargs.dataType = 'jsonp';
+		_ioargs.crossDomain = true;
+		doajax(_ioargs,_iocall);
+	}else if(mode == 'script'){
+		_ioargs.dataType = 'script';
+		_ioargs.crossDomain = true;
+		doajax(_ioargs,_iocall);
+	}
+}
+var mainqueue = new queueHandle(); //主线程队列控制对象
+module.exports = {
+	/**
+	 * 执行接口请求
+     * @param {JSON} *ioargs 接口扩展参数。对应ioconfig.js里的ioargs配置格式
+     * @param {JSON} *iocall 接口扩展参数。对应ioconfig.js里的iocall配置格式
+	 */
+	transRequest: function(ioargs,iocall){
+		if(ioargs && iocall && ioargs.url != ''){
+			var curopt = format(ioargs,iocall,mainqueue);
+			if(curopt.ioargs.customconfig.queue){ //说明遵循队列控制
+				mainqueue.request(curopt);
+			}
+			else{
+				request(curopt);
+			}
+		}
+	},
+	/**
+	 * 对一组请求进行单独的队列控制依次请求。全部请求完毕后进行通知。
+	 * 此情况下，通过ioargs设置的参数配置customconfig:{queue:true|false}无效。强制都走队列
+		 * @param {Array} *argsarr 接口请求数组
+		 * [{
+		 *    {JSON} *ioargs 接口扩展参数。对应ioconfig.js里的ioargs配置格式
+		 *    {JSON} *iocall 接口扩展参数。对应ioconfig.js里的iocall配置格式
+		 * }]
+		 * @param {JSON} customobj 用户自定义扩展参数
+		 * {
+		 * 	  {Function} complete 接口全部请求完毕后的通知回调
+		 * }
+	 */
+	transQueueRequest: function(argsarr,customobj){
+		if($.isArray(argsarr) && argsarr.length > 0){
+			var queueobj = queueControl.get(); //获取一个空闲的queueHandle
+			queueobj.complete = function(){
+				queueControl.end(queueobj); //通知当前queue对象使用完毕
+				if(customobj && typeof customobj.complete == 'function'){
+					customobj.complete();
+				}
+			};
+			for(var i = 0, len = argsarr.length; i < len; i++){
+				var ioargs = argsarr[i].ioargs || {};
+				var iocall = argsarr[i].iocall || {};
+				if(ioargs && iocall && ioargs.url != ''){
+					ioargs = $.extend(true,ioargs,{customconfig:{queue:true}});
+					var curopt = format(ioargs,iocall,queueobj);
+					queueobj.request(curopt);
+				}
+			}
+		}
+	}
+};
---
+++ io/ioconfig.js
@@ -0,0 +1,86 @@
+/**
+ * @fileoverview io接口请求相关数据配置
+ * @version 1.0 | 2015-06-28 版本信息
+ * @author Zhang Mingrui | 592044573@qq.com
+ * @return 更多详细信息参考代码里对应定义方法或属性位置的注释说明
+ * 	login {JSON} 对于接口返回未登陆错误进行统一处理配置
+ *  ioargs {JSON} io请求接口默认参数
+ *  setTrans {Function} 设置接口配置
+ *  getTrans {Function} 获取接口配置
+ *  globalSetup {Function} 设置全局ajax配置
+ * @example
+ *   const IoConfig = require('libio-ioconfig');
+ *
+ 	 //统一处理未登录
+	 IoConfig.login.filter = function(result){
+	 	return result.code == 'A0002';
+	 };
+	 IoConfig.login.url = 'http://baidu.com/';
+
+	 //所有接口的io业务错误统一处理
+	 IoConfig.fail.filter = function(result) {
+	 	return result.code != 'A0001';
+	 };
+	 IoConfig.iocall.fail = function(result){
+	 	alert(result.errmsg || '网络错误');
+	 };
+
+	 IoConfig.ioargs.crossDomain = true;
+ * */
+
+var that = {};
+/**
+ * 对于接口返回未登陆错误进行统一处理 配置。
+ */
+that.login = {
+	url: '', //未登情况下跳转的页面
+	key: 'go', //跳转到url指定页面传递当前页面地址的键值名称
+	deal: function(data){}, //有的情况下，不是跳转登录url，而且弹登录弹层。则此时将url设置为''，将登陆弹层弹出处理写在此方法中
+	filter: function(data){return false;} //如果此函数返回true则跳转url指定的页面。data是接口返回的数据
+};
+/**
+ * 对于接口返回的业务错误进行统一处理 配置。
+ * 如code == 'A0001' 算成功，其他都算失败
+ */
+that.fail = {
+    funname: 'fail', //当发生业务错误的时候，调用的格式同于ioargs里的函数的函数名。默认是error
+    filter: function(data){return false;} //如果此函数返回true则说明当前接口返回业务错误信息。data是接口返回的数据
+};
+that.ioargs = { //io请求默认的参数格式
+	//同ajax参数官方说明项
+	url: '',
+	method: 'GET',
+	contentType: 'application/x-www-form-urlencoded',
+	dealdata: function(result){return result.data;}, //当业务处理成功时，返回统一处理的数据
+	//自定义数据
+	customconfig:{
+		mode: 'ajax', //使用什么方式请求，默认是ajax(ajax方式默认返回的是json格式的数据。也可通过在和method参数同级的位置设置dataType参数来改变默认的json设置)。可用的参数有ajax|jsonp|script
+	    deallogin: true, //是否统一处理未登陆错误
+	    dealfail: true, //是否统一处理业务错误
+	    dealdata: true, //当业务处理成功时，是否统一处理返回的数据。注意：只有当dealerror为true时，dealdata为true才有效。否则不会调用dealdata方法
+	    queue: false, //接口请求是否进行队列控制，即当前请求完成后才可以进行下一个请求
+	    getInter: function(interobj){} //获取接口请求实例对象。如interobj为$.ajax()返回的对象
+	}
+};
+that.iocall = { //io请求回调
+	complete: function(){}, //参数为 data|jqXHR, textStatus, jqXHR|errorThrown
+	success: function(data, textStatus, jqXHR){},
+	error: function(jqXHR, textStatus, errorThrown){alert( textStatus || '网络错误'); },
+	fail: function(data, textStatus, jqXHR){} //当业务处理错误时，返回统一处理业务错误
+};
+/**
+ * 每个请求发送之前，统一格式化参数配置（格式同ioargs）。
+ * 应用场景： 每个业务项目需要配置统一的参数处理。
+ */
+that.format = function(opt){};
+/**
+ * 设置全局的接口请求配置
+ * @param {Object} setting
+ */
+that.globalSetup = function(setting){
+	if(typeof setting == 'object'){
+		$.ajaxSetup(setting);
+	}
+};
+
+module.exports = that;
